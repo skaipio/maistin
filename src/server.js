0daefe7b11a13f53b2b00912fb77572420ba166d
skaipio <senni.kaipio@gmail.com>
2017-08-26 16:36:33 +0300

    configure Webpack to create a bundle.js to contain browser-side code

--- src/server.js
+++ src/server.js
@@ -1,10 +1,13 @@
 import express from 'express';
+import path from 'path';
 import React from 'react';
 import ReactDOMServer from 'react-dom/server';
 import { getBeers } from './services/beerService';
 import BeerList from './components/BeerList';
 
 const app = express();
+
+const port = 8080;
 
 app.set('view engine', 'pug');
 
@@ -17,4 +20,8 @@ app.get('/', (req, res) => {
   });
 });
 
-app.listen(8080);
+app.use('/dist', express.static(path.join(__dirname, '../dist')));
+
+app.listen(port);
+
+console.info(`App listening on port ${port}`);
