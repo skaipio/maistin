84ff2d5fa50ccbad7e60746bbd6d0a6e4e06af27
skaipio <senni.kaipio@gmail.com>
2017-08-26 16:53:21 +0300

    Webpack watches for file changes

--- src/components/App.js
+++ src/components/App.js
@@ -4,4 +4,4 @@ import BeerList from './BeerList';
 import beers from '../services/beers.json';
 
 const targetElement = document.getElementById('main');
-ReactDOM.render(<BeerList beers={[]} />, targetElement);
+ReactDOM.render(<BeerList beers={beers} />, targetElement);
