d837ee0c3a3ef78f0947846f12818bdcb463aa78
skaipio <senni.kaipio@gmail.com>
2017-09-02 18:58:13 +0300

    Repair tests and add snapshot testing

--- src/components/__tests__/BeerList.test.js
+++ src/components/__tests__/BeerList.test.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import { ListGroupItem } from 'reactstrap';
+import renderer from 'react-test-renderer';
 import BeerList from '../BeerList';
 import beers from './beers.json';
 
@@ -12,10 +14,15 @@ describe('<BeerList />', () => {
     mockBeers = JSON.parse(JSON.stringify(beers));
   });
 
+  it('snapshot of beer list matches', () => {
+    const tree = renderer.create(<BeerList beers={mockBeers} />);
+    expect(tree).toMatchSnapshot();
+  });
+
   it('renders the beers passed to it', () => {
     const wrapper = shallow(<BeerList beers={mockBeers} />);
     beers.forEach((beer) => {
-      const beerItem = wrapper.find('li').filterWhere(li => keyEquals(li.key(), beer.id));
+      const beerItem = wrapper.find(ListGroupItem).filterWhere(li => keyEquals(li.key(), beer.id));
       expect(beerItem.length).toBe(1);
     });
   });
@@ -24,8 +31,9 @@ describe('<BeerList />', () => {
     const wrapper = shallow(<BeerList beers={mockBeers} />);
     beers.filter(beer => !beer.favorite)
       .forEach((beer) => {
-        const beerItem = wrapper.find('li').filterWhere(li => keyEquals(li.key(), beer.id)).first();
-        expect(beerItem.text()).not.toContain('<3');
+        const beerItem = wrapper.find(ListGroupItem)
+          .filterWhere(li => keyEquals(li.key(), beer.id)).first();
+        expect(beerItem.render().text()).not.toContain('<3');
       });
   });
 
@@ -33,26 +41,31 @@ describe('<BeerList />', () => {
     const wrapper = shallow(<BeerList beers={mockBeers} />);
     beers.filter(beer => beer.favorite)
       .forEach((beer) => {
-        const beerItem = wrapper.find('li').filterWhere(li => keyEquals(li.key(), beer.id)).first();
-        expect(beerItem.text()).toContain('<3');
+        const beerItem = wrapper.find(ListGroupItem)
+          .filterWhere(li => keyEquals(li.key(), beer.id)).first();
+        expect(beerItem.render().text()).toContain('<3');
       });
   });
 
   it('displays a heart icon after clicking on a beer without a heart icon', () => {
     const wrapper = shallow(<BeerList beers={mockBeers} />);
     const beerNotFavorited = 1;
-    let beerItem = wrapper.find('li').filterWhere(li => keyEquals(li.key(), beerNotFavorited)).first();
+    let beerItem = wrapper.find(ListGroupItem)
+      .filterWhere(li => keyEquals(li.key(), beerNotFavorited)).first();
     beerItem.simulate('click');
-    beerItem = wrapper.find('li').filterWhere(li => keyEquals(li.key(), beerNotFavorited)).first();
-    expect(beerItem.text()).toContain('<3');
+    beerItem = wrapper.find(ListGroupItem)
+      .filterWhere(li => keyEquals(li.key(), beerNotFavorited)).first();
+    expect(beerItem.render().text()).toContain('<3');
   });
 
   it('displays no heart icon after clicking on a beer with a heart icon', () => {
     const wrapper = shallow(<BeerList beers={mockBeers} />);
     const beerFavorited = 2;
-    let beerItem = wrapper.find('li').filterWhere(li => keyEquals(li.key(), beerFavorited)).first();
+    let beerItem = wrapper.find(ListGroupItem)
+      .filterWhere(li => keyEquals(li.key(), beerFavorited)).first();
     beerItem.simulate('click');
-    beerItem = wrapper.find('li').filterWhere(li => keyEquals(li.key(), beerFavorited)).first();
-    expect(beerItem.text()).not.toContain('<3');
+    beerItem = wrapper.find(ListGroupItem)
+      .filterWhere(li => keyEquals(li.key(), beerFavorited)).first();
+    expect(beerItem.render().text()).not.toContain('<3');
   });
 });
